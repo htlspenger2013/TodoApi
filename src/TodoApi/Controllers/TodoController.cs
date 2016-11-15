1e6c77bb30b6d31cc068ef7edd107746a3d6004b
Tschernko <tschernko@spengergasse.at>
2016-11-15 20:30:25 +0100

    Add project files.

---
+++ src/TodoApi/Controllers/TodoController.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using TodoApi.Models;
+
+// For more information on enabling Web API for empty projects, visit http://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace TodoApi.Controllers
+{
+    [Route("api/[controller]")]
+    public class TodoController : Controller
+    {
+        public ITodoRepository TodoItems { get; private set; }
+
+        public TodoController(ITodoRepository todoItems) {
+            TodoItems = todoItems;
+        }
+        // GET: api/values
+        [HttpGet]
+        public IEnumerable<TodoItem> GetAll()
+        {
+            return TodoItems.GetAll();
+        }
+
+        // GET api/values/5
+        [HttpGet("{id}", Name = "GetTodo")]
+        public IActionResult Get(string id)
+        {
+            var item = TodoItems.Find(id);
+            if (item == null) {
+                return NotFound();
+            }
+            return new ObjectResult(item);
+        }
+
+        // POST api/values
+        [HttpPost]
+        public void Post([FromBody]TodoItem value)
+        {
+        }
+
+        // PUT api/values/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody]TodoItem value)
+        {
+        }
+
+        // DELETE api/values/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
